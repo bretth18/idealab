7cd520280d8e6f7b736e2a9db34d71451c1c3126
Brett Henderson <bretth18@gmail.com>
2016-02-09 21:25:35 -0800

    updated login.js, register.js

--- client/templates/login.js
+++ client/templates/login.js
@@ -5,11 +5,21 @@ Template.login.events({
     var email = t.find('#login-email').value;
     var password = t.find('#login-password').value;
 
+    Meteor.autorun(function() {
+      //run function when session variable changes
+      var message = Session.get('displayMessage');
+      if(message) {
+        var stringArray = message.split('&');
+        Notifications.warn(stringArray[0], stringArray[1]);
+        Session.set('displayMessage', null);
+      }
+    });
+
     //assuming validation passes we pass to meteor.login func
     Meteor.loginWithPassword(email, password, function(err){
-      if(err)
+    //  if(err)
         //user not found
-      else
+    //  else
         //user logged in
     });
     return false;
--- client/templates/register.js
+++ client/templates/register.js
@@ -4,6 +4,27 @@ Template.register.events({
     var email = t.find('#account-email').value;
     var password = t.find('#account-password').value;
 
+    //validation
+
+    //trim whitespace from email
+    var trimInput = function(val) {
+      return val.replace(/^\s*|\s*$/g, "");
+    }
+    var email = trimInput(email);
+
+    //validate password to be atleast 6 chars
+    var isValidPassword = function(val, field) {
+      if(val.length >= 6){
+        return true;
+      }
+      else {
+        Session.set('displayMessage', 'Error: Too Short.')
+        return false;
+      }
+    }
+
+    //if(isValidPassword(userPassword))
+
     Accounts.createUser({email: email, password: password}),
     function(err){
       if (err) {
@@ -12,7 +33,7 @@ Template.register.events({
       else{
         //success, user account logged in
       }
-    });
+    }
     return false;
   }
 });
